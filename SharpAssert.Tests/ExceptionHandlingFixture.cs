831764ac1e11a24eb214f8c3e2965a1ca1de2125
Yevhen Bobrov <yevhen.bobrov@gmail.com>
2025-08-10 00:37:29 +0300

    Tidy up

--- SharpAssert.Tests/ExceptionHandlingFixture.cs
+++ SharpAssert.Tests/ExceptionHandlingFixture.cs
@@ -1,26 +1,11 @@
 using System.Collections;
 using System.Linq.Expressions;
-using System.Reflection;
-using FluentAssertions;
 
 namespace SharpAssert;
 
 [TestFixture]
 public class ExceptionHandlingFixture : TestBase
 {
-    [Test]
-    public void Should_handle_comparer_exceptions_in_EvaluateBinaryOperation()
-    {
-        var analyzerType = typeof(SharpInternal).Assembly.GetType("SharpAssert.ExpressionAnalyzer");
-        var method = analyzerType!.GetMethod("EvaluateBinaryOperation", 
-            BindingFlags.Static | BindingFlags.NonPublic);
-        
-        // Test with incompatible types that will cause Comparer<object>.Default.Compare to throw
-        var result = method!.Invoke(null, [ExpressionType.LessThan, new NonComparableClass(), new DifferentNonComparableClass()]);
-        
-        result.Should().Be(false);
-    }
-
     [Test]
     public void Should_handle_comparer_exceptions_in_real_expressions()
     {
